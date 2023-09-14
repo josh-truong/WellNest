9581b075d8f35fb45ef00ad2a694f6abf4403c9a
Joshua Truong <joshktruong@gmail.com>
2023-09-14 08:28:55 -0600

    Initial Commit

---
+++ WellNest/WellNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellNestApp.swift
+//  WellNest
+//
+//  Created by Joshua Truong on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct WellNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
