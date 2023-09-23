97bee77d688a9cc84a36f13235e429bcd5f58083
Joshua Truong <joshktruong@gmail.com>
2023-09-23 00:11:10 -0600

    Added dummy data for WgerExercise

---
+++ WellNest/TestData/WgerExerciseData.swift
@@ -0,0 +1,30 @@
+//
+//  WgerExerciseData.swift
+//  WellNest
+//
+//  Created by Joshua Truong on 9/22/23.
+//
+
+import Foundation
+
+class WgerExerciseData {
+    private let decoder: JSONDecoder = JSONDecoder()
+    
+    func getWgerExerciseDetail() -> WgerExerciseDetailModel {
+        let jsonData = """
+        {
+            "id": 210,
+            "base_id": 537,
+            "name": "Incline Bench Press - Dumbbell",
+            "category": "Chest",
+            "image": "/media/exercise-images/16/Incline-press-1.png",
+            "image_thumbnail": "/media/exercise-images/16/Incline-press-1.png.30x30_q85_crop-smart.png"
+        }
+        """.data(using: .utf8)!
+        do {
+            return try decoder.decode(WgerExerciseDetailModel.self, from: jsonData)
+        } catch {
+            return WgerExerciseDetailModel()
+        }
+    }
+}
