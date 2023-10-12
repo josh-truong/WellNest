8e0e27c1565e6e4edbbd519f6f9ab7c1c58cbed5
Joshua Truong <joshktruong@gmail.com>
2023-10-12 03:44:38 -0600

    Create GeoConnectModel.swift

---
+++ WellNest/Models/GeoConnectModel.swift
@@ -0,0 +1,14 @@
+//
+//  GeoConnectModel.swift
+//  WellNest
+//
+//  Created by Joshua Truong on 10/11/23.
+//
+
+import Foundation
+import MapKit
+
+struct GeoConnectModel {
+    var origin: CLLocationCoordinate2D
+    var region: MKCoordinateRegion
+}
