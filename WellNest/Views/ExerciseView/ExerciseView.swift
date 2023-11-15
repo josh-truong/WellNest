d22a6cd1381f8f261b3aba0e5e4ac617a54fb86b
Joshua Truong <joshktruong@gmail.com>
2023-11-15 01:09:02 -0700

    Update ExerciseView.swift

--- WellNest/Views/ExerciseView/ExerciseView.swift
+++ WellNest/Views/ExerciseView/ExerciseView.swift
@@ -18,7 +18,7 @@ struct ExerciseView: View {
                 if let categories = viewModel.exerciseDictionary, !categories.isEmpty {
                     List {
                         ForEach(categories.keys.sorted(), id: \.self) { key in
-                            ExerciseCategoryView(exerciseVM: viewModel, exerciseCategoryVM: ExerciseCategoryViewModel(apiService: APIService()),label: key, exercises: categories[key]!.exercises)
+                            ExerciseCategoryView(exerciseVM: viewModel, exerciseCategoryVM: ExerciseCategoryViewModel(apiService: APIService.shared),label: key, exercises: categories[key]!.exercises)
                         }
                     }
                     .listStyle(GroupedListStyle())
@@ -47,7 +47,7 @@ struct ExerciseView: View {
 
 struct Exercise_Previews: PreviewProvider {
     static var previews: some View {
-        ExerciseView(viewModel: ExerciseViewModel(apiService: APIService()))
+        ExerciseView(viewModel: ExerciseViewModel(apiService: APIService.shared))
     }
 }
 
