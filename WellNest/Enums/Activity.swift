c66da1a9f51390d3446ee5465334eb2e1b8e3e62
Joshua Truong <joshktruong@gmail.com>
2023-11-30 08:11:00 -0700

    Update to Codable (encodeable, decodable)

--- WellNest/Enums/Activity.swift
+++ WellNest/Enums/Activity.swift
@@ -8,110 +8,219 @@
 import Foundation
 import SwiftUI
 
-//  Activity.swift
-//  WellNest
-//
-//  Created by Joshua Truong on 11/27/23.
+struct CodableColor: Codable {
+    let red: Double
+    let green: Double
+    let blue: Double
+    let opacity: Double
 
-import Foundation
-import SwiftUI
-import SwiftData
+    init(_ color: Color) {
+        let uiColor = UIColor(color)
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
 
+        self.red = Double(red)
+        self.green = Double(green)
+        self.blue = Double(blue)
+        self.opacity = Double(alpha)
+    }
 
-protocol Activity {
-    var name: String { get }
-    var image: String { get }
-    var color: Color { get }
-    var unit: String { get }
+    var uiColor: Color {
+        return Color(red: red, green: green, blue: blue, opacity: opacity)
+    }
 }
 
-class ActivityInfo : Identifiable {
+class Activity: Identifiable, Codable {
+    var id: Int { return UUID().hashValue }
+    var name: String
+    var image: String
+    var color: CodableColor
+    var unit: String
+
+    init(name: String, image: String, color: CodableColor, unit: String) {
+        self.name = name
+        self.image = image
+        self.color = color
+        self.unit = unit
+    }
+
+    // If you have additional initialization logic after decoding, you can implement this method
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        image = try container.decode(String.self, forKey: .image)
+        color = try container.decode(CodableColor.self, forKey: .color)
+        unit = try container.decode(String.self, forKey: .unit)
+    }
+
+    // If you have additional encoding logic, you can implement this method
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(image, forKey: .image)
+        try container.encode(color, forKey: .color)
+        try container.encode(unit, forKey: .unit)
+    }
+
+    // CodingKeys enum to specify the coding keys
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case image
+        case color
+        case unit
+    }
+}
+
+
+class ActivityInfo: Codable {
     var id: Int
     var activity: Activity
     var start: Int
     var end: Int
-    
+
     init(activity: Activity, start: Int, end: Int) {
         self.id = UUID().hashValue
         self.activity = activity
         self.start = start
         self.end = end
     }
+
+    // If you want to customize the coding keys, you can implement the CodingKey enum
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case activity
+        case start
+        case end
+    }
+
+    // If you have additional initialization logic after decoding, you can implement this method
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        activity = try container.decode(Activity.self, forKey: .activity)
+        start = try container.decode(Int.self, forKey: .start)
+        end = try container.decode(Int.self, forKey: .end)
+    }
+
+    // If you have additional encoding logic, you can implement this method
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(activity, forKey: .activity)
+        try container.encode(start, forKey: .start)
+        try container.encode(end, forKey: .end)
+    }
 }
 
-struct Custom: Activity {
-    var name: String = "Custom"
-    let image: String = "figure"
-    let color: Color = Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1))
-    let unit: String = "minutes"
+class Custom: Activity {
+    init() {
+        super.init(name: "Custom", image: "figure", color: CodableColor(Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1))), unit: "minutes")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct Steps: Activity {
-    let name = "Today steps"
-    let image = "figure.walk"
-    let color = Color.green
-    let unit = "steps"
+class Steps: Activity {
+    init() {
+        super.init(name: "Today steps", image: "figure.walk", color: CodableColor(Color.green), unit: "steps")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct Water: Activity {
-    let name = "Glasses"
-    let image = "drop.fill"
-    let color = Color.blue
-    let unit = "glasses"
+class Water: Activity {
+    init() {
+        super.init(name: "Glasses", image: "drop.fill", color: CodableColor(Color.blue), unit: "glasses")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct Calories: Activity {
-    let name = "Calories"
-    let image = "flame"
-    let color = Color.red
-    let unit = "calories"
+class Calories: Activity {
+    init() {
+        super.init(name: "Calories", image: "flame", color: CodableColor(Color.red), unit: "calories")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct Running: Activity {
-    let name = "Running"
-    let image = "figure.run"
-    let color = Color.purple
-    let unit = "minutes"
+class Running: Activity {
+    init() {
+        super.init(name: "Running", image: "figure.run", color: CodableColor(Color.purple), unit: "minutes")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct WeightLifting: Activity {
-    let name = "Weight Lifting"
-    let image = "dumbbell"
-    let color = Color.cyan
-    let unit = "minutes"
+class WeightLifting: Activity {
+    init() {
+        super.init(name: "Weight Lifting", image: "dumbbell", color: CodableColor(Color.cyan), unit: "minutes")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct Soccer: Activity {
-    let name = "Soccer"
-    let image = "figure.soccer"
-    let color = Color.blue
-    let unit = "minutes"
+class Soccer: Activity {
+    init() {
+        super.init(name: "Soccer", image: "figure.soccer", color: CodableColor(Color.blue), unit: "minutes")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct Basketball: Activity {
-    let name = "Basketball"
-    let image = "figure.basketball"
-    let color = Color.orange
-    let unit = "minutes"
+class Basketball: Activity {
+    init() {
+        super.init(name: "Basketball", image: "figure.basketball", color: CodableColor(Color.orange), unit: "minutes")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct StairStepper: Activity {
-    let name = "Stair Stepper"
-    let image = "figure.stair.stepper"
-    let color = Color.green
-    let unit = "minutes"
+class StairStepper: Activity {
+    init() {
+        super.init(name: "Stair Stepper", image: "figure.stair.stepper", color: CodableColor(Color.green), unit: "minutes")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct Cycling: Activity {
-    let name = "Cycling"
-    let image = "figure.outdoor.cycle"
-    let color = Color.orange
-    let unit = "minutes"
+class Cycling: Activity {
+    init() {
+        super.init(name: "Cycling", image: "figure.outdoor.cycle", color: CodableColor(Color.orange), unit: "minutes")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
 
-struct Hiking: Activity {
-    let name = "Hiking"
-    let image = "figure.hiking"
-    let color = Color.mint
-    let unit = "minutes"
+class Hiking: Activity {
+    init() {
+        super.init(name: "Hiking", image: "figure.hiking", color: CodableColor(Color.mint), unit: "minutes")
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
 }
