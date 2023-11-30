84543eaddfb123e72d9ca901bdbc4700a20b605e
Joshua Truong <joshktruong@gmail.com>
2023-11-30 11:05:15 -0700

    messy refactor

--- WellNest/Core/Home/ActivityViewModel.swift
+++ WellNest/Core/Home/ActivityViewModel.swift
@@ -11,46 +11,72 @@ import SwiftUI
 
 @MainActor
 class ActivityViewModel : ObservableObject {
+    @Published var activeActivities = [ActivityInfo]()
+    @Published var inactiveActivities = [ActivityInfo]()
+    @Published var activities = [ActivityInfo]()
+    
     private let activeDS = DataService<ActivityInfo>(key: .activeActivities)
     private let activitiesDS = DataService<ActivityInfo>(key: .activities)
-    
-    private let preloadActivities: [ActivityInfo] = [
-        ActivityInfo(activity: Steps(), start: 0, end: 10000),
-        ActivityInfo(activity: Running(), start: 0, end: 10000),
-        ActivityInfo(activity: WeightLifting(), start: 0, end: 1000),
-    ]
-    
-    private let activities: [ActivityInfo] = [
-        ActivityInfo(activity: Steps(), start: 0, end: 10000),
-        ActivityInfo(activity: Running(), start: 0, end: 10000),
-        ActivityInfo(activity: WeightLifting(), start: 0, end: 1000),
-        ActivityInfo(activity: Soccer(), start: 0, end: 300),
-        ActivityInfo(activity: Basketball(), start: 0, end: 300),
-        ActivityInfo(activity: StairStepper(), start: 0, end: 300),
-        ActivityInfo(activity: Cycling(), start: 0, end: 300),
-        ActivityInfo(activity: Hiking(), start: 0, end: 300)
-    ]
     
     init() {
         setupDS()
     }
     
-    func getActivitiesInfo() -> [ActivityInfo] {
-        return activeDS.loadData() ?? [ActivityInfo]()
+    func getActiveActivities() {
+        self.activeActivities = activeDS.loadData() ?? [ActivityInfo]()
+    }
+    
+    func getInactiveActivities() {
+        guard let loadedActivity = activitiesDS.loadData() else { return }
+        guard let loadedActive = activeDS.loadData() else { return }
+        
+        let inactiveActivities = loadedActivity.filter { activity in
+            !loadedActive.contains { $0.id == activity.id }
+        }
+        self.inactiveActivities = inactiveActivities
+    }
+
+    func addActivityToActive(_ item: ActivityInfo) {
+        activeDS.addItem(item)
+        getInactiveActivities()
+        getActiveActivities()
     }
     
     func preload() {
+        let preloadActivities: [ActivityInfo] = [
+            ActivityInfo(activity: Steps(), start: 0, end: 10000),
+            ActivityInfo(activity: Running(), start: 0, end: 10000),
+            ActivityInfo(activity: WeightLifting(), start: 0, end: 1000),
+        ]
+        
         if let loadedActivity = activeDS.loadData(), loadedActivity.isEmpty {
             print("Preloading activities")
             activeDS.saveDataList(preloadActivities)
+            getActiveActivities()
         }
     }
     
     private func setupDS() {
         preload()
+        let activities: [ActivityInfo] = [
+            ActivityInfo(activity: Steps(), start: 0, end: 10000),
+            ActivityInfo(activity: Running(), start: 0, end: 10000),
+            ActivityInfo(activity: WeightLifting(), start: 0, end: 1000),
+            ActivityInfo(activity: Soccer(), start: 0, end: 300),
+            ActivityInfo(activity: Basketball(), start: 0, end: 300),
+            ActivityInfo(activity: StairStepper(), start: 0, end: 300),
+            ActivityInfo(activity: Cycling(), start: 0, end: 300),
+            ActivityInfo(activity: Hiking(), start: 0, end: 300)
+        ]
+        
         if let loadedActivity = activitiesDS.loadData(), loadedActivity.isEmpty {
             print("Storing activities")
             activitiesDS.saveDataList(activities)
         }
+    }
+    
+    func clearActives() {
+        activeDS.clearData()
+        getActiveActivities()
     }
 }
