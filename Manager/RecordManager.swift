d8e9302f5061623528f8945f63e4b39e492a5c7d
Joshua Truong <joshktruong@gmail.com>
2023-12-06 14:55:30 -0700

    core data, persistent container, model entity extensions

--- Manager/RecordManager.swift
+++ Manager/RecordManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import CoreData
+import SwiftUI
 
 
 //@objc(ActivityGroupRecord)
@@ -25,28 +26,22 @@ import CoreData
 //    @NSManaged public var end: Int
 //}
 
+
 class RecordManager: ObservableObject {
-    let container: NSPersistentContainer
+    let persistentContainer: NSPersistentContainer
     
     init() {
-        self.container = NSPersistentContainer(name: "FoodModel")
-        self.container.loadPersistentStores { description, error in
+        self.persistentContainer = NSPersistentContainer(name: "RecordModel")
+        self.persistentContainer.loadPersistentStores { description, error in
             if let error = error {
                 print("Failed to load the data \(error.localizedDescription)")
             }
         }
     }
-    
-    func saveFood(context: NSManagedObjectContext) {
-        do {
-            try context.save()
-            print("Data saved")
-        } catch {
-            print("uh oh")
-        }
-    }
-    
-    func addFood(item: WgerIngredientResult, context: NSManagedObjectContext) {
+}
+
+extension FoodEntity {
+    func add(item: WgerIngredientResult, context: NSManagedObjectContext) {
         let food = FoodEntity(context: context)
         food.timestamp = Date()
         food.id = Int64(item.id)
@@ -61,21 +56,78 @@ class RecordManager: ObservableObject {
         food.saturatedFatContent = Float(item.fatSaturated ?? "") ?? 0
         food.fibres = Float(item.fibres ?? "") ?? 0
         food.sodium = Float(item.sodium ?? "") ?? 0
-        saveFood(context: context)
-        
-        print(item.name)
+        save(context: context)
     }
     
-    func deleteFood(food: FoodEntity, context: NSManagedObjectContext) {
-        context.delete(food)
-        saveFood(context: context)
+    func delete(item: FoodEntity, context: NSManagedObjectContext) {
+        context.delete(item)
+        save(context: context)
     }
     
-    func editFood(food: FoodEntity, name: String, calories: String, context: NSManagedObjectContext) {
-        food.timestamp = Date()
-        food.name = name
-        food.energy = 0
+    func update(item: FoodEntity, name: String, calories: String, context: NSManagedObjectContext) {
+        item.timestamp = Date()
+        item.name = name
+        item.energy = 0
         
-        saveFood(context: context)
+        save(context: context)
+    }
+    
+    private func save(context: NSManagedObjectContext) {
+        do { try context.save() }
+        catch { print("Data not saved! \(error.localizedDescription)") }
+    }
+}
+
+
+extension ActivityEntity {
+    var activity: Activity {
+        return Activity(name: name ?? "", image: image ?? "", color: color?.uiColor ?? .clear, unit: unit ?? "")
+    }
+    
+    func add(name: String, image: String, unit: String, uiColor: Color, context: NSManagedObjectContext) {
+        let colorEntity = ColorEntity(context: context)
+        let rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) = colorEntity.rgba(color: uiColor)
+        colorEntity.red = rgba.red
+        colorEntity.green = rgba.green
+        colorEntity.blue = rgba.blue
+        colorEntity.alpha = rgba.alpha
+        
+        let entity = ActivityEntity(context: context)
+        entity.uuid = UUID()
+        entity.name = name
+        entity.image = image
+        entity.unit = unit
+        entity.color = colorEntity
+        
+        save(context: context)
+    }
+    
+    func addToRecords(elaspedTime: Date, context: NSManagedObjectContext) {
+        let entity = ActivityInfoEntity(context: context)
+        entity.uuid = UUID()
+        entity.timestamp = Date()
+        entity.elapsedTime = elaspedTime
+        
+        addToRecords(entity)
+        save(context: context)
+    }
+    
+    private func save(context: NSManagedObjectContext) {
+        do { try context.save() }
+        catch { print("Data not saved! \(error.localizedDescription)") }
+    }
+}
+
+extension ColorEntity {
+    var uiColor: Color {
+        return Color(red: red, green: green, blue: blue, opacity: alpha)
+    }
+    
+    func rgba(color: Color) -> (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        let uiColor = UIColor(color)
+        var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) = (0,0,0,0)
+        uiColor.getRed(&rgba.red, green: &rgba.green, blue: &rgba.blue, alpha: &rgba.alpha)
+        
+        return (rgba.red, rgba.green, rgba.blue, rgba.alpha)
     }
 }
