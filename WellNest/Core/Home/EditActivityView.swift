9fa6ccb530bd233ee49454317675213ed1588fb3
Joshua Truong <joshktruong@gmail.com>
2023-12-06 02:50:47 -0700

    Create EditActivityView.swift

---
+++ WellNest/Core/Home/EditActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  EditActivityView.swift
+//  WellNest
+//
+//  Created by Joshua Truong on 11/30/23.
+//
+
+import SwiftUI
+
+struct EditActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditActivityView()
+}
