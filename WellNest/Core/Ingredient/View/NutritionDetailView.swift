f479e3ba8e6adc93bb208b22a32497be4b86e41d
Joshua Truong <joshktruong@gmail.com>
2023-12-07 14:06:33 -0700

    Create NutritionDetailView.swift

---
+++ WellNest/Core/Ingredient/View/NutritionDetailView.swift
@@ -0,0 +1,33 @@
+//
+//  NutritionDetailView.swift
+//  WellNest
+//
+//  Created by Joshua Truong on 12/7/23.
+//
+
+import SwiftUI
+
+struct NutritionDetailView: View {
+    let food: FetchedResults<FoodEntity>.Element
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Name: \(food.name ?? "")")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Text("Energy: \(food.energy)")
+            Text("Protein: \(food.protein, specifier: "%.2f")")
+            Text("Carbohydrates: \(food.carbohydrates, specifier: "%.2f")")
+            Text("Carbohydrates Sugar: \(food.sugarCarbContent, specifier: "%.2f")")
+            Text("Fat: \(food.fat, specifier: "%.2f")")
+            Text("Fat Saturated: \(food.saturatedFatContent, specifier: "%.2f")")
+            Text("Fibres: \(food.fibres, specifier: "%.2f")")
+            Text("Sodium: \(food.sodium, specifier: "%.2f")")
+            
+            Spacer()
+        }
+        .padding()
+        .navigationBarTitle("Food Details", displayMode: .inline)
+    }
+}
