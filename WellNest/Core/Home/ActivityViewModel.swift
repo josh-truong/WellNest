4c624300845d0a1bead58abe8062d74625972765
Joshua Truong <joshktruong@gmail.com>
2023-12-07 01:50:36 -0700

    Update ActivityViewModel.swift

--- WellNest/Core/Home/ActivityViewModel.swift
+++ WellNest/Core/Home/ActivityViewModel.swift
@@ -7,67 +7,18 @@
 
 import Foundation
 import SwiftData
-import SwiftUI
+import CoreData
 
 @MainActor
 class ActivityViewModel : ObservableObject {
-    @Published var activeActivities = [ActivityInfo]()
-    @Published var inactiveActivities = [ActivityInfo]()
-    @Published var activities = [ActivityInfo]()
-    
-    private let activeDS = UserDefaultService<ActivityInfo>(key: .activeActivities)
-    private let activitiesDS = UserDefaultService<ActivityInfo>(key: .activities)
-    
-    init() {
-        setupDS()
-    }
-    
-    func getActiveActivities() {
-        self.activeActivities = activeDS.load() ?? [ActivityInfo]()
-    }
-    
-    func getInactiveActivities() {
-        guard let loadedActivity = activitiesDS.load() else { return }
-        guard let loadedActive = activeDS.load() else { return }
-        
-        let inactiveActivities = loadedActivity.filter { activity in
-            !loadedActive.contains { $0.id == activity.id }
-        }
-        self.inactiveActivities = inactiveActivities
-    }
-
-    func moveToActive(_ item: ActivityInfo) {
-        activeDS.add(item)
-        getInactiveActivities()
-        getActiveActivities()
-    }
-    
-    func moveToInctive(_ item: ActivityInfo) {
-        activeDS.remove(item)
-        getInactiveActivities()
-        getActiveActivities()
-    }
-    
-    func preload() {
-        let preloadActivities: [ActivityInfo] = [
+    func preload(context: NSManagedObjectContext) {
+        let preloadActiveActivities: [ActivityInfo] = [
             ActivityInfo(activity: Steps(), start: 0, end: 10000),
             ActivityInfo(activity: Running(), start: 0, end: 10000),
             ActivityInfo(activity: WeightLifting(), start: 0, end: 1000),
         ]
         
-        if let loadedActivity = activeDS.load(), loadedActivity.isEmpty {
-            print("Preloading activities")
-            activeDS.saveDataList(preloadActivities)
-            getActiveActivities()
-        }
-    }
-    
-    private func setupDS() {
-        preload()
-        let activities: [ActivityInfo] = [
-            ActivityInfo(activity: Steps(), start: 0, end: 10000),
-            ActivityInfo(activity: Running(), start: 0, end: 10000),
-            ActivityInfo(activity: WeightLifting(), start: 0, end: 1000),
+        let preloadInactiveActivities: [ActivityInfo] = [
             ActivityInfo(activity: Soccer(), start: 0, end: 300),
             ActivityInfo(activity: Basketball(), start: 0, end: 300),
             ActivityInfo(activity: StairStepper(), start: 0, end: 300),
@@ -75,26 +26,34 @@ class ActivityViewModel : ObservableObject {
             ActivityInfo(activity: Hiking(), start: 0, end: 300)
         ]
         
-        if let loadedActivity = activitiesDS.load(), loadedActivity.isEmpty {
-            print("Storing activities")
-            activitiesDS.saveDataList(activities)
+        if (UserDefaultService().getExecutedPreload()) {
+            do {
+                let existingEntities = try fetchAllEntities(context: context)
+                if existingEntities.isEmpty {
+                    print("Preloading activities")
+
+                    preloadActiveActivities.forEach { item in
+                        createActivityEntity(from: item, active: true, context: context)
+                    }
+                    
+                    preloadInactiveActivities.forEach { item in
+                        createActivityEntity(from: item, active: false, context: context)
+                    }
+                }
+                UserDefaultService().setExecutedPreload(false)
+            } catch {
+                print("Error fetching entities: \(error)")
+            }
         }
     }
     
-    func move(from fromOffsets: IndexSet, to toOffset: Int) {
-        activeDS.move(from: fromOffsets, to: toOffset)
-        getActiveActivities()
+    func fetchAllEntities(context: NSManagedObjectContext) throws -> [ActivityEntity] {
+        let fetchRequest: NSFetchRequest<ActivityEntity> = ActivityEntity.fetchRequest()
+        return try context.fetch(fetchRequest)
     }
     
-    func clearActives() {
-        activeDS.clear()
-        getActiveActivities()
-    }
-    
-    func delete(from fromOffsets: IndexSet) {
-        var deletedItems = fromOffsets.compactMap {
-            self.inactiveActivities[$0]
-        }
-        activitiesDS.remove(deletedItems.removeFirst())
+    func createActivityEntity(from activityInfo: ActivityInfo, active: Bool, context: NSManagedObjectContext) {
+        let activity = activityInfo.activity
+        ActivityEntity().add(name: activity.name, image: activity.image, unit: activity.unit, uiColor: activity.color, active: active, context: context)
     }
 }
