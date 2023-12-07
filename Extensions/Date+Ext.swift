9e81801af408c7eb258cacba03dbfd7438961161
Joshua Truong <joshktruong@gmail.com>
2023-12-07 14:08:15 -0700

    Update Date+Ext.swift

--- Extensions/Date+Ext.swift
+++ Extensions/Date+Ext.swift
@@ -15,4 +15,28 @@ extension Date {
     var endOfDay: Date {
         return Calendar.current.date(bySettingHour: 23, minute: 59, second: 59, of: Date()) ?? Date()
     }
+    
+    func relativeTime(in locale: Locale = .current) -> String {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .full
+        return formatter.localizedString(for: self, relativeTo: Date())
+    }
+    
+    func timeAgo(date: Date) -> String {
+        let minutes = Int(Date().timeIntervalSince(date)) / 60
+        
+        if minutes == 0 {
+            return "Just now"
+        } else if minutes < 60 {
+            return "\(minutes) minute\(minutes == 1 ? "" : "s") ago"
+        } else if minutes < 120 {
+            return "an hour ago"
+        } else if minutes < 1440 {
+            let hours = minutes / 60
+            return "\(hours) hour\(hours == 1 ? "" : "s") ago"
+        } else {
+            let days = minutes / 1440
+            return "\(days) day\(days == 1 ? "" : "s") ago"
+        }
+    }
 }
