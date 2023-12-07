d99462efc62a86da0476866a2361aa866fd62683
Joshua Truong <joshktruong@gmail.com>
2023-12-07 06:21:05 -0700

    Remove progress, added goal to activity

--- WellNest/Enums/Activity.swift
+++ WellNest/Enums/Activity.swift
@@ -14,13 +14,15 @@ class Activity: Identifiable {
     var image: String
     var color: Color
     var unit: String
+    var goal: Int
 
-    init(name: String, image: String, color: Color, unit: String) {
+    init(name: String, image: String, color: Color, unit: String, goal: Int) {
         self.id = UUID().hashValue
         self.name = name
         self.image = image
         self.color = color
         self.unit = unit
+        self.goal = goal
     }
 }
 
@@ -28,14 +30,12 @@ class Activity: Identifiable {
 class ActivityInfo: Identifiable, Equatable {
     var id: Int
     var activity: Activity
-    var start: Int
     var end: Int
 
-    init(activity: Activity, start: Int, end: Int) {
+    init(activity: Activity, goal: Int) {
         self.id = UUID().hashValue
         self.activity = activity
-        self.start = start
-        self.end = end
+        self.end = goal
     }
     
     static func == (lhs: ActivityInfo, rhs: ActivityInfo) -> Bool {
@@ -45,72 +45,72 @@ class ActivityInfo: Identifiable, Equatable {
 
 class Custom: Activity {
     init() {
-        super.init(name: "Custom", image: "figure", color: Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1)), unit: "minutes")
+        super.init(name: "Custom", image: "figure", color: Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1)), unit: "minutes", goal: 6000)
     }
 }
 
 class Steps: Activity {
     init() {
-        super.init(name: "Today steps", image: "figure.walk", color: Color.green, unit: "steps")
+        super.init(name: "Today steps", image: "figure.walk", color: Color.green, unit: "minutes", goal: 10000)
     }
 }
 
 class Water: Activity {
     init() {
-        super.init(name: "Glasses", image: "drop.fill", color: Color.blue, unit: "glasses")
+        super.init(name: "Glasses", image: "drop.fill", color: Color.blue, unit: "glasses", goal: 8)
     }
 }
 
 class Weight: Activity {
     init() {
-        super.init(name: "Weight", image: "scalemass.fill", color: Color.gray, unit: "lbs")
+        super.init(name: "Weight", image: "scalemass.fill", color: Color.gray, unit: "lbs", goal: 200)
     }
 }
 
 class Calories: Activity {
     init() {
-        super.init(name: "Calories", image: "flame", color: Color.red, unit: "calories")
+        super.init(name: "Calories", image: "flame", color: Color.red, unit: "calories", goal: 2500)
     }
 }
 
 class Running: Activity {
     init() {
-        super.init(name: "Running", image: "figure.run", color: Color.purple, unit: "minutes")
+        super.init(name: "Running", image: "figure.run", color: Color.purple, unit: "minutes", goal: 30)
     }
 }
 
 class WeightLifting: Activity {
     init() {
-        super.init(name: "Weight Lifting", image: "dumbbell", color: Color.cyan, unit: "minutes")
+        super.init(name: "Weight Lifting", image: "dumbbell", color: Color.cyan, unit: "minutes", goal: 250)
     }
 }
 
 class Soccer: Activity {
     init() {
-        super.init(name: "Soccer", image: "figure.soccer", color: Color.blue, unit: "minutes")
+        super.init(name: "Soccer", image: "figure.soccer", color: Color.blue, unit: "minutes", goal: 120)
     }
 }
 
 class Basketball: Activity {
     init() {
-        super.init(name: "Basketball", image: "figure.basketball", color: Color.orange, unit: "minutes")
+        super.init(name: "Basketball", image: "figure.basketball", color: Color.orange, unit: "minutes", goal: 120)
     }
 }
 
 class StairStepper: Activity {
     init() {
-        super.init(name: "Stair Stepper", image: "figure.stair.stepper", color: Color.green, unit: "minutes")
+        super.init(name: "Stair Stepper", image: "figure.stair.stepper", color: Color.green, unit: "minutes", goal: 30)
     }
 }
 
 class Cycling: Activity {
     init() {
-        super.init(name: "Cycling", image: "figure.outdoor.cycle", color: Color.orange, unit: "minutes")
+        super.init(name: "Cycling", image: "figure.outdoor.cycle", color: Color.orange, unit: "minutes", goal: 60)
     }
 }
 
 class Hiking: Activity {
     init() {
-        super.init(name: "Hiking", image: "figure.hiking", color: Color.mint, unit: "minutes")
+        super.init(name: "Hiking", image: "figure.hiking", color: Color.mint, unit: "minutes", goal:180)
     }
 }
