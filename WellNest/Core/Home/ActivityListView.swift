a08a7dd1af6fed4579c8a28a0eb09e57a4057b1c
Joshua Truong <joshktruong@gmail.com>
2023-12-07 18:25:50 -0700

    NSFetchRequest

--- WellNest/Core/Home/ActivityListView.swift
+++ WellNest/Core/Home/ActivityListView.swift
@@ -13,16 +13,50 @@ struct ActivityListView: View {
     @Environment(\.managedObjectContext) var managedObjContext
     @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "active == true")) var activities: FetchedResults<ActivityEntity>
     @FetchRequest(sortDescriptors: [SortDescriptor(\.timestamp, order: .reverse)]) var food: FetchedResults<FoodEntity>
+    @FetchRequest var water: FetchedResults<RecordEntity>
+    @FetchRequest var weight: FetchedResults<RecordEntity>
     
     @StateObject var vm = ActivityViewModel()
-    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    private let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    init() {
+        let fetchRequest: NSFetchRequest<RecordEntity> = RecordEntity.fetchRequest()
+        
+        var predicate = NSPredicate(
+            format: "name CONTAINS[cd] %@ AND timestamp >= %@ AND timestamp <= %@",
+            Weight().name,
+            Date().startOfDay as NSDate,
+            Date().endOfDay as NSDate
+        )
+        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = [ NSSortDescriptor(keyPath: \RecordEntity.timestamp, ascending: false) ]
+        fetchRequest.fetchLimit = 1
+        _weight = FetchRequest(fetchRequest: fetchRequest)
+        
+        predicate = NSPredicate(
+            format: "name CONTAINS[cd] %@ AND timestamp >= %@ AND timestamp <= %@",
+            Water().name,
+            Date().startOfDay as NSDate,
+            Date().endOfDay as NSDate
+        )
+        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = [ NSSortDescriptor(keyPath: \RecordEntity.timestamp, ascending: false) ]
+        fetchRequest.fetchLimit = 0
+        _water = FetchRequest(fetchRequest: fetchRequest)
+    }
     
     var body: some View {
         VStack {
-            ActivityCard(Weight(), start: 360, end: 200, showProgress: false)
+            NavigationLink(destination: EditActivityView(activity: Weight())) {
+                ActivityCard(Weight(), start: Int(weight.first?.quantity ?? 0), end: 200, showProgress: false)
+            }
+            .buttonStyle(PlainButtonStyle())
             ActivityCard(Calories(), start: vm.todaysCalories, end: 1000, showProgress: false)
                 .onAppear { vm.getTodaysCalories(context: managedObjContext) }
-            ActivityCard(Water(), start: 0, end: 8, showProgress: false)
+            NavigationLink(destination: EditActivityView(activity: Water())) {
+                ActivityCard(Water(), start: water.reduce(0) { (result, record) in return result + Int(record.quantity) }, end: 8, showProgress: false)
+            }
+            .buttonStyle(PlainButtonStyle())
             
             LazyVGrid(columns: columns) {
                 ForEach(activities) { info in
