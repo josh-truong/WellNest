995dbe1138b7fc630321f81924c5a1413597910a
Joshua Truong <joshktruong@gmail.com>
2023-12-07 18:27:45 -0700

    Input view and history view

--- WellNest/Core/Home/EditActivityView.swift
+++ WellNest/Core/Home/EditActivityView.swift
@@ -6,13 +6,53 @@
 //
 
 import SwiftUI
+import CoreData
 
-struct EditActivityView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct Entry: Codable, Identifiable {
+    var id = UUID()
+    var timestamp: Date
+    var input: Int
 }
 
-#Preview {
-    EditActivityView()
+struct EditActivityView: View {
+    @Environment(\.managedObjectContext) var managedObjContext
+    @Environment(\.dismiss) var dismiss
+    
+    @FetchRequest var entries: FetchedResults<RecordEntity>
+    @State private var activity: Activity
+    @State private var input: String = ""
+    
+    init(activity: Activity) {
+        _activity = State(initialValue: activity)
+        let predicate = NSPredicate(format: "name CONTAINS[cd] %@", activity.name)
+        let fetchRequest: NSFetchRequest<RecordEntity> = RecordEntity.fetchRequest()
+        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = [ NSSortDescriptor(keyPath: \RecordEntity.timestamp, ascending: false) ]
+        _entries = FetchRequest(fetchRequest: fetchRequest)
+    }
+    
+    var body: some View {
+        VStack {
+            TextField("Enter value", text: $input)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .keyboardType(.numberPad)
+
+            Button {
+                RecordEntity().add(name: activity.name, quantity: Int(input) ?? 0, context: managedObjContext)
+                dismiss()
+            } label: {
+                Text("Record")
+            }
+
+            // Display the entries
+            List(entries) { entry in
+                if let timestamp = entry.timestamp {
+                    Text("Timestamp: \(timestamp.formatted()), Input: \(entry.quantity)")
+                } else {
+                    Text("Invalid Timestamp")
+                }
+            }
+        }
+    }
 }
