2c0c74170709d29bba6d753dba5662e0e6e289ff
Joshua Truong <joshktruong@gmail.com>
2023-12-08 16:35:56 -0700

    Create ChartView.swift

---
+++ WellNest/Components/ChartView.swift
@@ -0,0 +1,15 @@
+//
+//  ChartView.swift
+//  WellNest
+//
+//  Created by Joshua Truong on 12/7/23.
+//
+
+import SwiftUI
+import SwiftUICharts
+
+struct ChartView: View {
+    var body: some View {
+        Text("")
+    }
+}
