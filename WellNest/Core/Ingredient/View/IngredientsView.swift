0b041bbe26c2523d78043c7ba362072dcddb3517
Joshua Truong <joshktruong@gmail.com>
2023-12-13 12:31:43 -0700

    UI/UX Update

--- WellNest/Core/Ingredient/View/IngredientsView.swift
+++ WellNest/Core/Ingredient/View/IngredientsView.swift
@@ -10,22 +10,37 @@ import SwiftUI
 struct IngredientsView: View {
     @StateObject var vm: IngredientViewModel = .init()
     @State private var searchTerm: String = ""
+    @State private var isViewSearch: Bool = true
      
     var body: some View {
-        NavigationView {
-            VStack {
-                if (searchTerm.isEmpty) {
+        VStack {
+            Picker("", selection: $isViewSearch) {
+                Text("All").tag(true)
+                Text("Custom").tag(false)
+            }
+            .pickerStyle(.segmented)
+            .padding(.horizontal, 50)
+            .padding(.bottom)
+            if (isViewSearch) {
+                if searchTerm.isEmpty {
                     DefaultIngredientView()
                 } else {
                     SearchIngredientsView(results: $vm.results)
                 }
+            } else {
+                Text("Custom View")
             }
+            Spacer()
         }
         .navigationTitle("Ingredients")
         .searchable(text: $searchTerm)
+        .onChange(of: isViewSearch) { oldState, newState in
+            if !isViewSearch { searchTerm.removeAll() }
+        }
         .onChange(of: searchTerm) { oldState, newState in
             Task {
                 if !newState.isEmpty && newState.count > 2 {
+                    isViewSearch = true
                     await vm.searchIngredient(term: newState)
                 } else {
                     vm.results.removeAll()
