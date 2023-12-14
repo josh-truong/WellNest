ad5e1fd89203870a437365d30547e87714ad73ff
Joshua Truong <joshktruong@gmail.com>
2023-12-14 11:26:57 -0700

    Firestore realtime code

--- Manager/FirebaseManager.swift
+++ Manager/FirebaseManager.swift
@@ -1,20 +1,67 @@
-//
-//  FirebaseManager.swift
-//  WellNest
-//
-//  Created by Joshua Truong on 11/1/23.
-//
-
+// FirebaseManager.swift
 import Foundation
-import FirebaseCore
 import FirebaseFirestore
-import FirebaseDatabase
+import FirebaseFirestoreSwift
 
-class FirebaseManager : ObservableObject {
-    var ref: DatabaseReference!
-    
-    init() {
-        ref = Database.database().reference()
-        self.ref.setValue(["counter": 1])
+struct FriendActivity: Identifiable, Encodable, Decodable {
+    var id: Int { return UUID().hashValue }
+    var name: String = ""
+    var image: String = ""
+    var start: Int = 0
+    var end: Int = 0
+    var unit: String = ""
+}
+
+class FirebaseManager: ObservableObject {
+    private var db = Firestore.firestore()
+    @Published var activities: [FriendActivity] = []
+
+    private var listener: ListenerRegistration?
+
+    func startListening(user: User) {
+        listener = Firestore.firestore().collection("activities").document(user.id)
+            .addSnapshotListener { snapshot, error in
+                guard let snapshot = snapshot else {
+                    print("Error fetching snapshot: \(error?.localizedDescription ?? "Unknown error")")
+                    return
+                }
+
+                if let activity = try? snapshot.data(as: FriendActivity.self) {
+                    if let index = self.activities.firstIndex(where: { $0.id == activity.id }) {
+                        self.activities[index] = activity
+                    } else {
+                        self.activities.append(activity)
+                    }
+                }
+            }
+    }
+
+    func stopListening() {
+        listener?.remove()
+    }
+
+    func addActivity(user: User, activity: FriendActivity) async {
+        if let activity = activity.toDictionary {
+            do {
+                try await Firestore.firestore().collection("activities").document(user.id).setData(activity)
+            } catch {
+                print("\(error.localizedDescription)")
+            }
+        }
+    }
+
+    func deleteActivity(user: User, activityID: String) async {
+        try? await Firestore.firestore().collection("activities").document(user.id).delete()
+    }
+
+    func fetchActivities(user: User) async {
+        do {
+            let snapshot = try await Firestore.firestore().collection("activities").document(user.id).getDocument()
+            let activity = try snapshot.data(as: FriendActivity.self)
+            self.activities = [activity]
+        } catch {
+            self.activities = []
+            print("Error fetching activities: \(error.localizedDescription)")
+        }
     }
 }
