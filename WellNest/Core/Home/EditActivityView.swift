11b285ec80f3eeeaeb1c9b5207ba6db0115d44c5
Joshua Truong <joshktruong@gmail.com>
2023-12-13 23:39:23 -0700

    Update EditActivityView.swift

--- WellNest/Core/Home/EditActivityView.swift
+++ WellNest/Core/Home/EditActivityView.swift
@@ -27,43 +27,42 @@ struct EditActivityView: View {
     
     var body: some View {
         VStack {
-            TextField("Enter value", text: $input)
-                .multilineTextAlignment(.center)
-                .font(.title)
-                .padding(.horizontal)
-                .padding(.top, 12)
-                .keyboardType(.numberPad)
-                .onChange(of: input) { input = input.filter { "0123456789".contains($0) } }
-            
-            HStack {
-                Spacer()
-                Button {
-                    Task {
-                        RecordEntity().add(name: activity.name, quantity: Int(input) ?? 0, context: managedObjContext)
-                        dismiss()
-                    }
-                } label: {
-                    Text("RECORD")
-                        .foregroundColor(.white)
-                        .frame(width: UIScreen.main.bounds.width-200, height: 48)
-                }
-                .background(Color(.systemBlue))
-                .disabled(!formIsValid)
-                .opacity(formIsValid ? 1.0 : 0.5)
-                .cornerRadius(10)
-                .padding(.top, 24)
-                Spacer()
+            VStack {
+                TextField("Enter value", text: $input)
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    .padding(.horizontal)
+                    .padding(.top, 12)
+                    .padding(.bottom, 12) // Add bottom padding here
+                    .keyboardType(.numberPad)
+                    .onChange(of: input) { input = input.filter { "0123456789".contains($0) } }
             }
+            .background(activity.color.opacity(0.4))
 
             List {
                 ForEach(entries) { entry in
                     if let timestamp = entry.timestamp {
-                        Text("Timestamp: \(timestamp.formatted()), Input: \(entry.quantity)")
+                        HStack {
+                            Text("\(timestamp.formatted())")
+                            Spacer()
+                            Text("\(entry.quantity) \(activity.unit)")
+                        }
                     }
                 }
             }
+            .listStyle(.plain)
         }
         .navigationTitle("Record \(activity.name)")
+        .toolbar {
+            ToolbarItem {
+                Button("add", action: {
+                    RecordEntity().add(name: activity.name, quantity: Int(input) ?? 0, context: managedObjContext)
+                    dismiss()
+                })
+                .disabled(!formIsValid)
+                .opacity(formIsValid ? 1.0 : 0.5)
+            }
+        }
     }
 }
 
