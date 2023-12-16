6d050ca6660f25c0521be6d502df2dbd28503798
Joshua Truong <joshktruong@gmail.com>
2023-12-16 11:12:24 -0700

    Create MealtimeNutrientModel.swift

---
+++ WellNest/Core/Ingredient/Model/MealtimeNutrientModel.swift
@@ -0,0 +1,16 @@
+//
+//  MealtimeNutrientModel.swift
+//  WellNest
+//
+//  Created by Joshua Truong on 12/16/23.
+//
+
+import Foundation
+
+struct MealtimeNutrientModel {
+    var goal : Int = 0
+    var calories: Int = 0
+    var protein: CGFloat = 0.0
+    var carbs: CGFloat = 0.0
+    var fat: CGFloat = 0.0
+}
