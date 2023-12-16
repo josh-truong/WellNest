6869d7e5cba89b36a671f2aebe4cb0e00a3c3d3e
Joshua Truong <joshktruong@gmail.com>
2023-12-16 11:12:43 -0700

    Create MealtimeType.swift

---
+++ WellNest/Enums/MealtimeType.swift
@@ -0,0 +1,41 @@
+//
+//  MealtimeType.swift
+//  WellNest
+//
+//  Created by Joshua Truong on 12/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+protocol MealtimeProtocol {
+    var name: MealtimeType { get }
+    var image: String { get }
+    var color: Color { get }
+}
+
+enum MealtimeType : String {
+    case breakfast = "Breakfast"
+    case lunch = "Lunch"
+    case dinner = "Dinner"
+}
+
+struct Breakfast : MealtimeProtocol {
+    let name: MealtimeType = .breakfast
+    let image: String = "sun.horizon"
+    let color: Color = .yellow
+}
+
+
+struct Lunch : MealtimeProtocol {
+    let name: MealtimeType = .lunch
+    let image: String = "sun.max"
+    let color: Color = .blue
+}
+
+
+struct Dinner : MealtimeProtocol {
+    let name: MealtimeType = .dinner
+    let image: String = "moon.haze"
+    let color: Color = .purple
+}
