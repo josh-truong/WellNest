f5810e98633f86bc2f3ff7eb13c59b0492cc7b27
Joshua Truong <joshktruong@gmail.com>
2023-12-17 14:03:55 -0700

    view update for nutrition

--- WellNest/Core/Ingredient/View/NutritionView.swift
+++ WellNest/Core/Ingredient/View/NutritionView.swift
@@ -9,54 +9,29 @@ import SwiftUI
 import CoreData
 
 struct NutritionView: View {
-    @Environment(\.managedObjectContext) var managedObjContext
     @FetchRequest(sortDescriptors: [SortDescriptor(\.timestamp, order: .reverse)]) var entities: FetchedResults<FoodEntity>
-    
-    private let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
-    @State private var showDetails: Bool = false
-    @State private var selectedEntity: FetchedResults<FoodEntity>.Element?
     @StateObject private var vm: NutritionViewModel = .init()
-    @State private var selectedDate: Date = Date().startOfDay
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading) {
-                DayCapsulePicker(dateRange: vm.sortedDates, selection: $selectedDate)
-                    .onChange(of: selectedDate) { vm.setDate(selectedDate) }
-                    .onAppear { vm.setDate(selectedDate) }
+                DayCapsulePicker(dateRange: vm.sortedDates, selection: $vm.selectedDate)
+                    .onChange(of: vm.selectedDate) { vm.setDate(vm.selectedDate) }
                 Text("\(Int(totalCaloriesToday())) kcal (Today)")
                 ScrollView {
-                    MealtimeCard(type: Breakfast(), info: vm.breakfast, destination: IngredientsView())
-                    MealtimeCard(type: Lunch(), info: vm.lunch, destination: IngredientsView())
-                    MealtimeCard(type: Dinner(), info: vm.dinner, destination: IngredientsView())
+                    MealtimeCard(type: Breakfast(), info: $vm.breakfast, destination: IngredientsView())
+                    MealtimeCard(type: Lunch(), info: $vm.lunch, destination: IngredientsView())
+                    MealtimeCard(type: Dinner(), info: $vm.dinner, destination: IngredientsView())
                 }
-                //                List {
-                //                    ForEach(entities) { entity in
-                //                        VStack(alignment: .leading, spacing: 6) {
-                //                            Text(entity.name ?? "")
-                //                                .lineLimit(2)
-                //                                .truncationMode(.tail)
-                //                                .bold()
-                //                            HStack {
-                //                                Text("\(entity.energy)") +
-                //                                Text(" kcal").foregroundStyle(.red)
-                //                                Spacer()
-                //                                Text(entity.timestamp ?? Date(), style: .date)
-                //                                    .foregroundStyle(.gray)
-                //                                    .italic()
-                //                            }
-                //                        }
-                //                        .onTapGesture {
-                //                            selectedEntity = entity
-                //                            showDetails.toggle()
-                //                        }
-                //                    }
-                //                    .onDelete(perform: deleteFood)
-                //                }
-                //                .listStyle(.plain)
-                
             }
-            .onAppear() { vm.organizeMeals(entities) }
+            .onReceive(timer) { _ in vm.organizeMeals(entities) }
+            .onAppear() { 
+                vm.setDate(vm.selectedDate)
+                vm.organizeMeals(entities)
+            }
+            .onChange(of: vm.sortedDates) { vm.setDate(vm.selectedDate) }
+            .onDisappear() { vm.selectedDate = Date() }
             .navigationTitle("Nutrition")
             .toolbar {
                 ToolbarItem {
@@ -65,18 +40,7 @@ struct NutritionView: View {
                     }
                 }
             }
-            .sheet(isPresented: $showDetails) {
-                if let selectedEntity = selectedEntity {
-                    NutritionDetailView(food: selectedEntity)
-                }
-            }
             .padding()
-        }
-    }
-    
-    private func deleteFood(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { entities[$0] }.forEach { FoodEntity().delete(item: $0, context: managedObjContext) }
         }
     }
     
