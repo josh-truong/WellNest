aa07a5bada556e0e89b369154dd2a423d9d5aa94
Joshua Truong <joshktruong@gmail.com>
2023-12-17 18:13:36 -0700

    Create NutrientSettingsView.swift

---
+++ WellNest/Core/Ingredient/View/NutrientSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  NutrientSettingsView.swift
+//  WellNest
+//
+//  Created by Joshua Truong on 12/17/23.
+//
+
+import SwiftUI
+
+struct NutrientSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NutrientSettingsView()
+}
