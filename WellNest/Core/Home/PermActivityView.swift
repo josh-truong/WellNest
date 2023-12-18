dbf9351821d3c7e58cf552cb7f78552743812905
Joshua Truong <joshktruong@gmail.com>
2023-12-18 01:06:30 -0700

    updated ui/ux with sliders

--- WellNest/Core/Home/PermActivityView.swift
+++ WellNest/Core/Home/PermActivityView.swift
@@ -54,30 +54,69 @@ struct PermActivityView: View {
         _steps = FetchRequest(fetchRequest: fetchRequest)
     }
     
+    @State var entity: FetchRequest<RecordEntity>?
+    @State var value: Float = 0
+    @State var showWater: Bool = false
+    @State var showWeight: Bool = false
+    
+    @State var stepGoal: Int = 0
+    @State var weightGoal: Int = 0
+    @State var waterGoal: Int = 0
+    
     var body: some View {
         VStack {
             NavigationLink(destination: GeoConnectView().navigationBarTitleDisplayMode(.inline)) {
-                ActivityCard(Steps(), start: Int(steps.first?.quantity ?? 0), end: 200, showProgress: false)
+                ActivityCard(Steps(), start: Int(steps.first?.quantity ?? 0), end: stepGoal, showProgress: false)
+                    .onDisappear() { getGoals() }
             }
             .buttonStyle(PlainButtonStyle())
             
             HStack {
-                NavigationLink(destination: EditActivityView(activity: Weight())) {
-                    ActivityCard(Weight(), start: Int(weight.first?.quantity ?? 0), end: 200, showProgress: false, disableName: true)
-                }
-                .buttonStyle(PlainButtonStyle())
+                ActivityCard(Weight(), start: Int(weight.first?.quantity ?? 0), end: weightGoal, showProgress: false, disableName: true)
+                    .onTapGesture { showWeight.toggle() }
+                    .sheet(isPresented: $showWeight, content: {
+                        RecordEntitySlider(activity: Weight(), min: 0, max: 300)
+                            .presentationDetents([.height(200)])
+                            .presentationCornerRadius(12)
+                            .onDisappear() { getGoals() }
+                    })
                 
-                NavigationLink(destination: EditActivityView(activity: Water())) {
-                    ActivityCard(Water(), start: water.reduce(0) { (result, record) in return result + Int(record.quantity) }, end: 8, showProgress: false, disableName: true)
-                }
-                .buttonStyle(PlainButtonStyle())
+                ActivityCard(Water(), start: water.reduce(0) { (result, record) in return result + Int(record.quantity) }, end: waterGoal, showProgress: false, disableName: true)
+                    .onTapGesture { showWater.toggle() }
+                    .sheet(isPresented: $showWater, content: {
+                        RecordEntitySlider(activity: Water(), min: 0, max: 10)
+                            .presentationDetents([.height(200)])
+                            .presentationCornerRadius(12)
+                            .onDisappear() { getGoals() }
+                    })
             }
             
             NavigationLink(destination: NutritionView()) {
-                ActivityCard(Calories(), start: vm.todaysCalories, end: 1000, showProgress: false)
-                    .onAppear { vm.getTodaysCalories(context: managedObjContext) }
+                ActivityCard(Calories(), start: vm.todaysCalories, end: Int(vm.calories), showProgress: false)
+                    .onAppear {
+                        vm.getTodaysCalories(context: managedObjContext)
+                        vm.getTotalCalories()
+                    }
             }
             .buttonStyle(PlainButtonStyle())
         }
+        .onAppear() { getGoals() }
+    }
+    
+    func getGoals() {
+        stepGoal = getGoal(Steps())
+        weightGoal = getGoal(Weight())
+        waterGoal = getGoal(Water())
+    }
+    
+    func getGoal(_ activity: Activity) -> Int {
+        if UserDefaults.standard.object(forKey: activity.name) == nil {
+            setGoal(activity, value: activity.goal)
+        }
+        return UserDefaults.standard.integer(forKey: activity.name)
+    }
+
+    func setGoal(_ activity: Activity, value: Int) {
+        UserDefaults.standard.set(value, forKey: activity.name)
     }
 }
