4b45f31319bb18809c3d4ff55accd264f687c218
Joshua Truong <joshktruong@gmail.com>
2023-12-18 01:06:53 -0700

    reactive ui

--- WellNest/Core/Ingredient/View/NutritionView.swift
+++ WellNest/Core/Ingredient/View/NutritionView.swift
@@ -14,28 +14,62 @@ struct NutritionView: View {
     @State private var showSettings: Bool = false
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
+    @State var cals: Float = 0
+    @State var carbs: Float = 0
+    @State var proteins: Float = 0
+    @State var fats: Float = 0
+    
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading) {
                 DayCapsulePicker(dateRange: vm.sortedDates, selection: $vm.selectedDate)
                     .onChange(of: vm.selectedDate) { vm.setDate(vm.selectedDate) }
-                Text("\(Int(totalCaloriesToday())) cal (Today)")
+                Text("\(vm.breakfast.calories + vm.lunch.calories + vm.dinner.calories) cal (Today)")
                 ScrollView {
+                    ZStack {
+                        Color(uiColor: .systemGray6)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                        
+                        VStack {
+                            HStack {
+                                ProgressBar(name: "Calories", start: Float(vm.breakfast.calories + vm.lunch.calories + vm.dinner.calories), goal: cals, color: .red)
+                                ProgressBar(name: "Carbs", start: Float(vm.breakfast.carbs + vm.lunch.carbs + vm.dinner.carbs), goal: carbs, color: .blue)
+                            }
+                            
+                            HStack {
+                                ProgressBar(name: "Protein", start: Float(vm.breakfast.protein + vm.lunch.protein + vm.dinner.protein), goal: proteins, color: .green)
+                                ProgressBar(name: "Fat", start: Float(vm.breakfast.fat + vm.lunch.fat + vm.dinner.fat), goal: fats, color: .yellow)
+                            }
+                            .padding(.top)
+                        }
+                        .padding()
+                    }
+                    
                     MealtimeCard(type: Breakfast(), info: $vm.breakfast, destination: IngredientsView())
                     MealtimeCard(type: Lunch(), info: $vm.lunch, destination: IngredientsView())
                     MealtimeCard(type: Dinner(), info: $vm.dinner, destination: IngredientsView())
                 }
+                .scrollIndicators(.hidden)
             }
             .onReceive(timer) { _ in vm.organizeMeals(entities) }
-            .onAppear() { 
+            .onAppear() {
                 vm.setDate(vm.selectedDate)
                 vm.organizeMeals(entities)
+                
+                cals = Float(NutrientGoalsSettingsManager.getNutrientGoal(nutrient: .breakfastCal))
+                    + Float(NutrientGoalsSettingsManager.getNutrientGoal(nutrient: .lunchCal))
+                    + Float(NutrientGoalsSettingsManager.getNutrientGoal(nutrient: .dinnerCal))
+                carbs = Float(NutrientGoalsSettingsManager.getNutrientGoal(nutrient: .carbs))
+                proteins = Float(NutrientGoalsSettingsManager.getNutrientGoal(nutrient: .proteins))
+                fats = Float(NutrientGoalsSettingsManager.getNutrientGoal(nutrient: .fats))
             }
             .onChange(of: vm.sortedDates) { vm.setDate(vm.selectedDate) }
             .onDisappear() { vm.selectedDate = Date() }
             .navigationTitle("Nutrition")
             .sheet(isPresented: $showSettings, content: {
-                NutrientSettingsView()
+                NutrientsSettingsView()
+                    .presentationDetents([.height(700)])
+                    .presentationCornerRadius(12)
             })
             .toolbar {
                 ToolbarItem {
@@ -56,15 +90,5 @@ struct NutritionView: View {
             }
             .padding()
         }
-    }
-    
-    private func totalCaloriesToday() -> Int {
-        var total: Int = 0
-        for item in entities {
-            if Calendar.current.isDateInToday(item.timestamp!) {
-                total += Int(item.energy)
-            }
-        }
-        return total
     }
 }
