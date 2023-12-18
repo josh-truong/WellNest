33f074a22b0f6d28e0eb8dcb9c91e55345f23e23
Joshua Truong <joshktruong@gmail.com>
2023-12-17 18:13:55 -0700

    Update MealtimeType.swift

--- WellNest/Enums/MealtimeType.swift
+++ WellNest/Enums/MealtimeType.swift
@@ -12,6 +12,7 @@ protocol MealtimeProtocol {
     var name: MealtimeType { get }
     var image: String { get }
     var color: Color { get }
+    var goal: Int { get }
 }
 
 enum MealtimeType : String, CaseIterable {
@@ -24,6 +25,7 @@ struct Breakfast : MealtimeProtocol {
     let name: MealtimeType = .breakfast
     let image: String = "sun.horizon"
     let color: Color = .yellow
+    let goal: Int = NutrientGoalsSettingsManager.getNutrientGoal(nutrient: .breakfastCal)
 }
 
 
@@ -31,6 +33,7 @@ struct Lunch : MealtimeProtocol {
     let name: MealtimeType = .lunch
     let image: String = "sun.max"
     let color: Color = .blue
+    let goal: Int = NutrientGoalsSettingsManager.getNutrientGoal(nutrient: .lunchCal)
 }
 
 
@@ -38,4 +41,5 @@ struct Dinner : MealtimeProtocol {
     let name: MealtimeType = .dinner
     let image: String = "moon.haze"
     let color: Color = .purple
+    let goal: Int = NutrientGoalsSettingsManager.getNutrientGoal(nutrient: .dinnerCal)
 }
